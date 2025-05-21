c5c8e08afbe31e2fb7402e9190627681346de3ef
Guilherme Castelão <guilherme@castelao.net>
2025-05-20 22:39:44 -0600

    Reinforcing code style (#9)
    * style: Activating Ruff's rule D
    
    * style: style: Activating Ruff's rule E
    
    * style: Activating Ruff's rule W
    
    * style: Activating Ruff's rule Q
    
    * style: Activating Ruff's rule FURB
    
    * style: Activating Ruff's rule N
    
    * style: Activating Ruff's rule PD
    
    * style: Activating Ruff's rule PERF
    
    * style: Activating Ruff's rule PTH
    
    * style: Activating Ruff's rule RET
    
    * style: Activating Ruff's rule RUF
    
    * style: Activating Ruff's rule SIM
    
    * style: Activating Ruff's rule TRY
    
    * style: Activating Ruff's rule PL
    
    * cfg: Ignore Ruff formating on legacy code
    
    * style:
    
    * cfg: Bump Ruff's version used in GA
    
    * cfg: Checking format as well with Ruff
    
    We don't want to change too much the legacy modules to make it easier on
    everyone. By ignoring the legacy parts we can guarantee that the new
    code is well formated.
    
    * fix: Loading version from dynamically defined _version.py
    
    * style: UTF-8 encoding declaration is unnecessary
    
    * fix: Correct path to load version
    
    * cfg: Updating Ruff action used to check format

--- geoPFA/__init__.py
+++ geoPFA/__init__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 """
 GeoPFA modeling software.
 """
-import numpy as np
-np.seterr(all='ignore')
 
-# from .version import __version__
-# Temporary setup
-__version__ = '0.0.1'
+import numpy as np
+
+np.seterr(all="ignore")
+
+from ._version import __version__
